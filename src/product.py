8aedb9fddad47b63918f8f3cb90f57f012dcca87
Dmitrii Grechko <prime.optimys@mail.ru>
2025-02-21 17:23:08 +0500

    after linters

--- src/product.py
+++ src/product.py
@@ -10,9 +10,8 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
-
     @classmethod
-    def new_product(cls, new_product: dict, products_list: list=None):
+    def new_product(cls, new_product: dict, products_list: list = None):
         if list(new_product.keys()) == ["name", "description", "price", "quantity"]:
             name, description, price, quantity = new_product.values()
             if products_list:
@@ -26,11 +25,9 @@ class Product:
         else:
             raise ValueError("Входные данные не корректны")
 
-
     @property
     def price(self):
         return self.__price
-
 
     @price.setter
     def price(self, new_price: float):
@@ -38,8 +35,13 @@ class Product:
             print("Цена не должна быть нулевая или отрицательная")
         else:
             if new_price < self.__price:
-                if input("Подтвердите операцию снижения цены - \"y\" считается за согласие,"
-                         "а любой другой ответ отменяет действие: ") == "y":
+                if (
+                    input(
+                        'Подтвердите операцию снижения цены - "y" считается за согласие,'
+                        "а любой другой ответ отменяет действие: "
+                    )
+                    == "y"
+                ):
                     self.__price = float(new_price)
                 else:
                     print("Отмена действия")
