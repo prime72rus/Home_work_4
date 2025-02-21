4f635b0d6844c89bae19f28303b0d47981c24113
Dmitrii Grechko <prime.optimys@mail.ru>
2025-02-21 17:20:54 +0500

    add setter and getter price in product.py

--- src/main.py
+++ src/main.py
@@ -26,10 +26,10 @@ if __name__ == "__main__":
     print(new_product.price)
     print(new_product.quantity)
 
-    # new_product.price = 800
-    # print(new_product.price)
-    #
-    # new_product.price = -100
-    # print(new_product.price)
-    # new_product.price = 0
-    # print(new_product.price)
+    new_product.price = 800
+    print(new_product.price)
+
+    new_product.price = -100
+    print(new_product.price)
+    new_product.price = 0
+    print(new_product.price)
--- src/product.py
+++ src/product.py
@@ -7,8 +7,9 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
 
     @classmethod
     def new_product(cls, new_product: dict, products_list: list=None):
@@ -24,3 +25,23 @@ class Product:
             return cls(name, description, price, quantity)
         else:
             raise ValueError("Входные данные не корректны")
+
+
+    @property
+    def price(self):
+        return self.__price
+
+
+    @price.setter
+    def price(self, new_price: float):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            if new_price < self.__price:
+                if input("Подтвердите операцию снижения цены - \"y\" считается за согласие,"
+                         "а любой другой ответ отменяет действие: ") == "y":
+                    self.__price = float(new_price)
+                else:
+                    print("Отмена действия")
+            else:
+                self.__price = float(new_price)
