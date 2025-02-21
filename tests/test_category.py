2d00e75d1b1d0b03d55e3231188bfd7ac26b5ded
Dmitrii Grechko <prime.optimys@mail.ru>
2025-02-21 20:02:11 +0500

    add tests function

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,10 +1,18 @@
 def test_category_init(bakery_category, milky_category):
     assert bakery_category.name == "Хлебобулочные изделия"
     assert bakery_category.description == "Хлеб и выпечка"
-    assert len(bakery_category.products) == 2
+    assert (
+        bakery_category.products
+        == "Батон французский, 55.0 руб. Остаток: 5 шт.\nХлеб ржаной, 61.0 руб. Остаток: 7 шт.\n"
+    )
 
     assert bakery_category.category_count == 2
     assert bakery_category.product_count == 4
 
     assert milky_category.category_count == 2
     assert milky_category.product_count == 4
+
+
+def test_add_product(milky_category, milky_product_2):
+    milky_category.add_product(milky_product_2)
+    assert milky_category.category_count == 3
